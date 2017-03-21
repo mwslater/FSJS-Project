be0d1cf7776f9f5227cca088c5dda626a01273b6
mwslater <mwslater@kent.edu>
2017-03-20 22:51:45 -0400

    Style work on the front end. Added express and mongoose to the project.  Started preliminary DB work.

---
+++ public/app.js
@@ -0,0 +1,15 @@
+(function () {
+    angular
+        .module("BlogApp", [])
+        .controller("BlogController", BlogController);
+
+    function BlogController($scope, $http) {
+        $scope.createPost = createPost;
+
+        function createPost(post) {
+            console.log(post);
+            $http.post("/api/blogpost", post);
+        }
+
+    }
+})();
--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mattblog');
+
+var PostSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    body: String,
+    tag: {type: String, enum: ['POLITICS', 'ECONOMY', 'EDUCATION']},
+    posted: {type: Date, default: Date.now}
+}, {collection: 'post'});
+
+var PostModel = mongoose.model("PostModel", PostSchema);
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+app.post("/api/blogpost", createPost);
+
+function createPost(req, res) {
+    var post = req.body;
+    console.log(post);
+    PostModel.create(post);
+    res.json(post);
+}
 
 app.listen(3000);
